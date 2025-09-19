c8315b52552acb724a094fa3ca7c5aabe7ff3a8e
Yoda <yoda@example.com>
2025-09-19 14:31:03 +0300

    create config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,17 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        #postgresql+asyncpg://db_user:db_pass:@db_host:db_port/db_name
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
