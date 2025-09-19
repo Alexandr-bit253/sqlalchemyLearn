e9cba4c8ad0ec0bffe49c95ee49a741bd4ad3eb1
Yoda <yoda@example.com>
2025-09-19 16:55:14 +0300

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import asyncio
 import os
 import sys
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+from queries.core import create_tables
+
+if __name__ == "__main__":
+    asyncio.run(create_tables())
--- src/queries/core.py
+++ src/queries/core.py
@@ -16,4 +16,5 @@ async def print_version_postgresql():
 
 async def create_tables():
     async with engine.begin() as conn:
+        await conn.run_sync(metadata_obj.drop_all)
         await conn.run_sync(metadata_obj.create_all)
