bac5c81bbe0f90641e8ad6e3be3cc9c7066e29bd
Yoda <yoda@example.com>
2025-09-19 16:30:02 +0300

    create table worker

--- src/database.py
+++ src/database.py
@@ -6,21 +6,16 @@ from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from config import settings
 
-engine: AsyncEngine = create_async_engine(
-    url=settings.DATABASE_URL_asyncpg,
-    echo=True,
-    pool_size=5,
-    max_overflow=10,
-)
+engine: AsyncEngine = create_async_engine(url=settings.DATABASE_URL_asyncpg, echo=False)
 
 
-async def test_connection():
+async def print_version_postgresql():
     async with engine.connect() as conn:
         result: CursorResult[Any] = await conn.execute(text("SELECT VERSION()"))
-        version = result.scalar_one()
+        version = result.scalar()
         print(version)
     await engine.dispose()
 
 
 if __name__ == "__main__":
-    asyncio.run(test_connection())
+    asyncio.run(print_version_postgresql())
---
+++ src/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, MetaData, String, Table
+
+metadata_obj: MetaData = MetaData()
+
+workers_table: Table = Table(
+    "worker",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("id", String),
+)
